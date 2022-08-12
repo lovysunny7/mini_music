a9e6165010e08e213439e72c4829b7c247b6d3b2
hyexjun <hyebyjun@gmail.com>
2022-08-12 21:51:35 +0900

    Feat : 로그인/회원가입 state 테스트

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,37 +1,65 @@
+import { useState } from 'react';
 import { Button, Form, Modal } from 'react-bootstrap';
 
 const Login = ({ login, handleCloseLogin }) => {
+  const [state, setState] = useState({
+    loginId: '',
+    loginPw: '',
+  });
+
+  const handleChangeState = (event) => {
+    console.log(event.target.name);
+    console.log(event.target.value);
+    setState({
+      ...state,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleClose = () => {
+    handleCloseLogin(); // 실제 모달 닫는 함수
+    setState({
+      loginId: '',
+      loginPw: '',
+    }); // 인풋 초기화
+  };
   return (
     <>
-      <Modal show={login} onHide={handleCloseLogin}>
+      <Modal show={login} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Log In</Modal.Title>
+          <Modal.Title>로그인</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
-            <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
-              <Form.Label>ID</Form.Label>
+            <Form.Group className='mb-3' controlId='loginId'>
+              <Form.Label>아이디</Form.Label>
               <Form.Control
                 type='id'
                 placeholder='아이디를 입력하세요.'
                 autoFocus
+                name='loginId'
+                value={state.loginId}
+                onChange={handleChangeState}
               />
             </Form.Group>
-            <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
-              <Form.Label>Password</Form.Label>
+            <Form.Group className='mb-3' controlId='loginPw'>
+              <Form.Label>비밀번호</Form.Label>
               <Form.Control
                 type='password'
                 placeholder='비밀번호를 입력하세요.'
                 autoFocus
+                name='loginPw'
+                value={state.loginPw}
+                onChange={handleChangeState}
               />
             </Form.Group>
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant='secondary' onClick={handleCloseLogin}>
+          <Button variant='secondary' onClick={handleClose}>
             Close
           </Button>
-          <Button variant='primary' onClick={handleCloseLogin}>
+          <Button variant='primary' onClick={() => {}}>
             Log In
           </Button>
         </Modal.Footer>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,44 +1,73 @@
+import { useState } from 'react';
 import { Button, Form, Modal } from 'react-bootstrap';
 
 const SignUp = ({ signup, handleCloseSignup }) => {
+  const [state, setState] = useState({
+    signupId: '',
+    signupName: '',
+    signupPw1: '',
+    signupPw2: '',
+  });
+
+  const handleChangeState = (event) => {
+    console.log(event.target.name);
+    console.log(event.target.value);
+    setState({
+      ...state,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <>
       <Modal show={signup} onHide={handleCloseSignup}>
         <Modal.Header closeButton>
-          <Modal.Title>Sign Up</Modal.Title>
+          <Modal.Title>회원 가입</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
-            <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
+            <Form.Group className='mb-3' controlId='signupId'>
               <Form.Label>아이디</Form.Label>
               <Form.Control
                 type='id'
-                placeholder='4자 이상 숫자 어쩌구 조건'
+                placeholder='아이디를 입력하세요.'
                 autoFocus
+                name='signupId'
+                value={state.signupId}
+                onChange={handleChangeState}
               />
             </Form.Group>
-            <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
+            <Form.Group className='mb-3' controlId='signupName'>
               <Form.Label>사용자명</Form.Label>
               <Form.Control
                 type='name'
                 placeholder='닉네임'
                 autoFocus
+                name='signupName'
+                value={state.signupName}
+                onChange={handleChangeState}
               />
             </Form.Group>
-            <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
+            <Form.Group className='mb-3' controlId='signupPw1'>
               <Form.Label>비밀번호</Form.Label>
               <Form.Control
                 type='password'
                 placeholder='비밀번호 조건 어쩌구'
                 autoFocus
+                name='signupPw1'
+                value={state.signupPw1}
+                onChange={handleChangeState}
               />
             </Form.Group>
-            <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
+            <Form.Group className='mb-3' controlId='signupPw2'>
               <Form.Label>비밀번호 확인</Form.Label>
               <Form.Control
                 type='password'
                 placeholder='비밀번호를 동일하게 한 번 더 입력하세요.'
                 autoFocus
+                name='signupPw2'
+                value={state.signupPw2}
+                onChange={handleChangeState}
               />
             </Form.Group>
           </Form>
