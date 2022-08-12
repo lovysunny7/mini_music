4816ea98df182f8464493c3ab321b788bdf532f6
hyexjun <hyebyjun@gmail.com>
2022-08-12 21:49:05 +0900

    Chore : html class 변경

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,17 +1,15 @@
 const MusicCard = () => {
   return (
-    <div class='col'>
-      <div class='card'>
+    <div className='col'>
+      <div className='card'>
         <img
           src='https://w.namu.la/s/a5cb942717e4f4fe8c00b62622d9694398a2ce19b7ca7bba7f51dab7651a6ff060352acd01bdc704952fd8aeaf07a53b91728595c1ec8f002ae57a66180786fd731069c278995fa168663fa45df495231c343763be48aebd9748141f074c697b'
-          class='card-img-top'
+          className='card-img-top'
           alt='앨범 이미지 설명글'
         />
-        <div class='card-body'>
-          <h5 class='card-title'>Card title</h5>
-          <p class='card-text'>
-            This is a longer card with supporting text...
-          </p>
+        <div className='card-body'>
+          <h5 className='card-title'>Card title</h5>
+          <p className='card-text'>This is a longer card with supporting text...</p>
         </div>
       </div>
     </div>
