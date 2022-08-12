1ce1e0cecc7cb0cdfc50a2f14fdd27588c9dfe85
hyexjun <hyebyjun@gmail.com>
2022-08-12 15:58:54 +0900

    Feat : 메인페이지 카드 추가 테스트

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <MusicCard />
+    </>
+  );
+};
+
+export default Home;
