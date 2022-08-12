1c1d4e17cc26bb8c1a3390a9e86557abdd97d52a
hyexjun <hyebyjun@gmail.com>
2022-08-12 17:31:38 +0900

    Chore : 홈페이지 html 배치요소 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,33 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
 import Header from '../components/Header';
 import MusicCard from '../components/MusicCard';
 
 const Home = () => {
+  // const dispatch = useDispatch();
+  const [logIn, setLogIn] = useState(false);
+  const [signUp, setSignUp] = useState(false);
+
   return (
     <>
       <Header />
-      <MusicCard />
+      <StWrap>
+        <div class='row row-cols-1 row-cols-md-4 g-3'>
+          <MusicCard />
+          <MusicCard />
+          <MusicCard />
+          <MusicCard />
+          <MusicCard />
+        </div>
+      </StWrap>
     </>
   );
 };
+
+const StWrap = styled.div`
+  /* border: 1px solid rebeccapurple; */
+  margin: 50px 100px;
+`;
 
 export default Home;
