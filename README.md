7053b65fdd99c7c396db1b1753b5746b546f3ddc
SUNNY-LAPTOP\Sunny <lovysunny7@gmail.com>
2022-08-13 21:42:34 +0900

    Docs: #6 Fix .md file

--- README.md
+++ README.md
@@ -5,8 +5,10 @@
 - yarn add react-redux @reduxjs/toolkit
 - yarn add styled-component
 - yarn add react-player
+- yarn add json-db
 
-<img src="https://img.shields.io/badge/git-F05032?style=for-the-badge&logo=git&logoColor=white"><img src="https://img.shields.io/badge/react-61DAFB?style=for-the-badge&logo=react&logoColor=black"> 
+<img src="https://img.shields.io/badge/git-F05032?style=for-the-badge&logo=git&logoColor=white"><img src="https://img.shields.io/badge/react-61DAFB?style=for-the-badge&logo=react&logoColor=black"><img src="https://img.shields.io/badge/bootstrap-7952B3?style=for-the-badge&logo=bootstrap&logoColor=white"><img src="https://img.shields.io/badge/github-181717?style=for-the-badge&logo=github&logoColor=white">
+
 
 ## Naming Convention
 Component 파일명은 pascal case 
---
+++ src/shared/Cookie.jsx
@@ -0,0 +1,27 @@
+const getCookie = (name) => {
+    // 쿠키 값을 가져옵니다.
+    let value = "; " + document.cookie;
+    // 키 값을 기준으로 파싱합니다.
+    let parts = value.split("; " + name + "=");
+    // value를 return!
+    if (parts.length === 2) {
+          return parts.pop().split(";").shift();
+      }
+  };
+  
+  // 쿠키에 저장하는 함수
+  const setCookie = (name, value, exp = 5) => {
+    let date = new Date();
+    // 날짜를 만들어줍니다.
+    date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
+    // 저장!
+    document.cookie = `${name}=${value};expires=${date.toUTCString()};path=/`;
+  };
+  
+  // 만료일을 예전으로 설정해 쿠키를 지웁니다.
+  const deleteCookie = (name) => {
+    let date = new Date("2020-01-01").toUTCString();
+    document.cookie = name + "=; expires=" + date;
+  }
+  
+  export { getCookie, setCookie, deleteCookie };
