0b2c0086856d8a12b621049a3738b09d58b24a6a
hyexjun <hyebyjun@gmail.com>
2022-08-14 15:28:45 +0900

    Chore : db 좋아요 int로 변경 및 엄격모드 비활성화

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
-import store from './redux/store'
+import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
+  // <React.StrictMode>
+  <Provider store={store}>
     <App />
-    </Provider>
-  </React.StrictMode>
+  </Provider>
+  // </React.StrictMode>
 );
-
