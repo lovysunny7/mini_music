0273887fdc618577d3f842acef77f6f15cc85bc6
hyexjun <hyebyjun@gmail.com>
2022-08-14 15:30:25 +0900

    Css : 반응형 가로너비 테스트 조정

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,10 +4,17 @@ const Layout = (props) => {
   return <StLayout>{props.children}</StLayout>;
 };
 
-const StLayout = styled.div`
+export const StLayout = styled.div`
   /* max-width: 1200px;
   min-width: 800px; */
   margin: 5% 15%;
+  /* border: 1px solid red; */
+`;
+
+export const StDetailWrap = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  /* border: 1px solid rebeccapurple; */
 `;
 
 export default Layout;
