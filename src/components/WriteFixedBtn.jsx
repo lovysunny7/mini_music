3e91a0e7ab24fcfdbd5cbcdb24a8a81620bd496c
hyexjun <hyebyjun@gmail.com>
2022-08-16 22:32:11 +0900

    Feat : 메인 글쓰기버튼 로그인 시에만 되도록 처리 #3 url로 접근시 현재는 접근 가능

--- src/components/WriteFixedBtn.jsx
+++ src/components/WriteFixedBtn.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import { getCookie } from '../shared/Cookie';
 
 const WriteFixedBtn = () => {
+  const cookie = getCookie('accessToken');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (cookie !== undefined) {
+      return setIsLoggedIn(true);
+    }
+  }, []);
+
   const navigate = useNavigate();
   return (
     <div>
@@ -21,7 +31,9 @@ const WriteFixedBtn = () => {
           cursor: PointerEvent,
         }}
         onClick={() => {
-          navigate('/postwrite');
+          isLoggedIn
+            ? navigate('/postwrite')
+            : alert('로그인 사용자만 접근이 가능합니다!');
         }}
       >
         <span style={{ fontSize: '36px' }} className='material-icons'>
--- src/pages/Home2.jsx
+++ src/pages/Home2.jsx
@@ -6,11 +6,10 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { updateIshidden } from '../redux/modules/postSlice';
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import ViewModal from '../components/ViewModal';
-import { __getOnePost,__getAll } from '../redux/asyncThunk/asyncPost';
+import { __getOnePost, __getAll } from '../redux/asyncThunk/asyncPost';
 import apis from '../api/axios';
-
 
 const Home2 = () => {
   const navigate = useNavigate();
@@ -23,55 +22,59 @@ const Home2 = () => {
   //   setPosts(data);
   // };
   const showAll = () => {
-   apis.post_all().then((res)=> 
-   {console.log(res.data)
-   setPosts(res.data)})
-  }
-  
+    apis.post_all().then((res) => {
+      console.log(res.data);
+      setPosts(res.data);
+    });
+  };
+
   // const payload = dispatch(__getAll()).then((res)=>{return res});
 
   const payload2 = async () => {
-    const {data} = await dispatch(__getAll());
+    const { data } = await dispatch(__getAll());
     console.log(data);
-  }
+  };
 
-  
   useEffect(() => {
     showAll();
     // payload2();
     // setPosts(dispatch(__getAll()));
-
   }, []);
-  
+
   const [show, setShow] = useState(false);
-  const handleShow = () => {setShow(true)};
+  const handleShow = () => {
+    setShow(true);
+  };
   const handleClose = () => setShow(false);
-  const handleModal = (post) =>{
+  const handleModal = (post) => {
     // console.log(post);
     handleShow();
     setPost(post);
     // return (
     //     <ViewModal show={show} handleShow={handleShow} handleClose={handleClose} post={post}/>
     // )
-  }
-  
+  };
+
   const onClickPost = (post) => {
     // dispatch(__getOnePost(post));
-  }
+  };
 
   return (
     <>
       <StLayout>
         <StSecTitle>All Genre 🎂</StSecTitle>
-        <ViewModal show={show} handleShow={handleShow} handleClose={handleClose} post={post}/>
+        <ViewModal
+          show={show}
+          handleShow={handleShow}
+          handleClose={handleClose}
+          post={post}
+        />
         <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 g-4'>
           {posts.map((post) => (
             // <div onClick={()=>navigate(`/posts/${post.postId}`)}>
             // <div onClick={()=>navigate(`/postview/${post.postId}`)}>
-            <div  key={post.postId} onClick={()=>handleModal(post)}>
-            <PostCard
-              post={post}
-            />
+            <div key={post.postId} onClick={() => handleModal(post)}>
+              <PostCard post={post} />
             </div>
           ))}
         </div>
