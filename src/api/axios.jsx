bd53accc77c4a6ce776e8bafd54155119304f2eb
SUNNY-LAPTOP\Sunny <lovysunny7@gmail.com>
2022-08-17 00:56:00 +0900

    Chore,Test: #6 현재 서버에 포스트 가능!

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -8,13 +8,13 @@ export const api = axios.create({
     // credentials:true,  
 })
 
-export const apiForm = axios.create({
-  baseURL: 'http://52.78.235.109/api',
-  headers: {
-    'Content-Type': 'multipart/form-data',
-  },
-  crdentials:true,
-});
+// export const apiForm = axios.create({
+//   baseURL: 'http://52.78.235.109/api',
+//   headers: {
+//     'Content-Type': 'multipart/form-data',
+//   },
+//   credentials:true,
+// });
 
 // 2. request interceptor
 api.interceptors.request.use(
@@ -49,7 +49,7 @@ const apis = {
   post_all: async () => await api.get('/posts'),
  // db 용
  post_view2:  async (postId) => await api.get(`/posts/${postId}`,postId),
- post_write2: async (payload) => await apiForm.post('/posts', payload),
+ post_write2: async (payload) => await api.post('/posts', payload),
  post_del2:  async (postId) => await api.delete(`/posts/${postId}`,postId),
 
   // local용
